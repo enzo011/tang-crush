392932e2dd03c7140a9bba2d2bdddf071bf779fc
enzo011 <85271423+enzo011@users.noreply.github.com>
2021-08-11 23:15:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Hẹn một ngày không xa cùng đi cafe nhá.',
+    messDesc: 'Hẹn một ngày không xa gặp nhỉ.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
