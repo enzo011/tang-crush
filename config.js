58af8062504330c46e0f00094ce9ac3bfe18dc1a
enzo011 <85271423+enzo011@users.noreply.github.com>
2022-05-14 22:56:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'My Friend à!',
+    introTitle: 'Chị yêu à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao cậu cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao chị cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng tớ đã say ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng tớ đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là say cậu <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao tớ lại say cậu <3',
-    btnReply: 'Gửi cho cậu <3',
+    title: 'Phải chăng em đã say ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là say chị <33',
+    btnNo: 'Không, Mập à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại say chị <3',
+    btnReply: 'Gửi cho chị <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Tớ biết mà 🥰. Say cậu nhiều nhiều 😘😘',
+    mess: 'Em biết mà 🥰. Say chị nhiều nhiều 😘😘',
     messDesc: 'Hẹn một ngày không xa gặp nhỉ.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://playerduo.com/messages/60f70100ec9bc77571cb0999'
+    messLink: 'https://www.facebook.com/hgswn.28/'
 }
